4e6787a47fb19f1fee7699e45059e02e39bde0b4
Jared Goodwin <38594033+goodwin72@users.noreply.github.com>
2019-07-09 22:47:14 -0700

    Cleared node_modules and reran npm install; preliminary createTournament route working

--- api/routes/tournaments.js
+++ api/routes/tournaments.js
@@ -1,6 +1,10 @@
+//Requires - Node
 var express = require('express');
 var router = express.Router();
 var pool = require('../connectionPool');
+
+//Requires - Local
+const helpers = require('../helpers');
 
 router.route('/')
   .get(getTournaments)
@@ -23,7 +27,26 @@ function getTournaments(req, res){
 }
 
 function createTournament(req, res){
-
+  if(helpers.checkIfLoggedIn(req)){
+    //If the request gave a name for the tournament...
+    if(req.body.name){
+      pool.query('INSERT INTO tournaments (name) VALUES ($1)', 
+          [req.body.name], (error, results) => {
+          if (error) {
+            res.status(400).send(error);
+          }
+          else{
+            res.status(200).json(results.rows); //empty?
+          }
+        });
+    }
+    else{
+      res.status(400).send("No tournament name given");
+    }
+  }
+  else{
+    res.status(400).send("Not logged in");
+  }
 }
 
 function getTournament(req, res){
